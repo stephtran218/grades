c23cebb8d6eb83a4fed3a6e832872ab22e508102
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-02-02 10:21:36 -0800

    2/2

--- studentGrades/studentGrades/main.swift
+++ studentGrades/studentGrades/main.swift
@@ -3,10 +3,15 @@
 //  studentGrades
 //
 //  Created by StudentAM on 1/29/24.
-//
+
 
 import Foundation
 import CSV
+
+var studentNames: [String] = []
+var studentGrades: [[String]] = []
+var finalGrades: [Double] = []
+var numOfAssignments: Int = 0
 
 do{
     let stream = InputStream(fileAtPath: "/Users/studentam/Desktop/grades.csv" )
@@ -19,11 +24,6 @@ do{
 catch{
     print("There was an error trying to read the file")
 }
-
-var studentNames: [String] = []
-var studentGrades: [[String]] = []
-var finalGrades: [Double] = []
-var numOfAssignments: Int = 0
 
 func manageData( _ studentInfo: [String]) {
     var tempGrades: [String] = []
@@ -40,22 +40,22 @@ func manageData( _ studentInfo: [String]) {
 
 mainMenu()
 
-func mainMenu(){
+func mainMenu() {
     var menuChoice: String = "0"
-    while menuChoice != "9"{
+    while menuChoice != "9" {
         print("Welcome to the Grade Manager!\n",
-        "What would you like to do? (Enter the number):\n",
-        "1. Display grade of a single student\n",
-        "2. Display all grades for a student\n",
-        "3. Display all grades of ALL students\n",
-        "4. Find the average grade of the class\n",
-        "5. Find the average grade of an assignment\n",
-        "6. Find the lowest grade in the class\n",
-        "7. Find the highest grade of the class\n",
-        "8. Filter students by grade range\n",
-        "9. Quit\n")
+              "What would you like to do? (Enter the number):\n",
+              "1. Display grade of a single student\n",
+              "2. Display all grades for a student\n",
+              "3. Display all grades of ALL students\n",
+              "4. Find the average grade of the class\n",
+              "5. Find the average grade of an assignment\n",
+              "6. Find the lowest grade in the class\n",
+              "7. Find the highest grade of the class\n",
+              "8. Filter students by grade range\n",
+              "9. Quit\n")
         
-        if let userPick = readLine(){
+        if let userPick = readLine() {
             if userPick == "1" {
                 displaySingleStudentGrade()
             } else if userPick == "2" {
@@ -75,42 +75,40 @@ func mainMenu(){
             } else if userPick == "9" {
                 quitMenu()
             } else {
-                print ("Please enter a valid selection 1-9")
+                print("Please enter a valid selection 1-9")
             }
             menuChoice = userPick
         }
     }
 }
 
-func displaySingleStudentGrade(){
-    print ("Which student would you like to choose?")
+func displaySingleStudentGrade() {
+    print("Which student would you like to choose?")
     
-    if let userPick = readLine(){
+    if let userPick = readLine() {
         findStudent(userPick)
     }
 }
 
-func findStudent(_ userPick: String) {
-    for i in studentNames.indices {
-        if studentNames[i] == userPick {
-            let foundStudent = studentNames[i]
-            calcFinalGrade(foundStudent, studentGrades, finalGrades, tempGrades)
+func findStudent( _ userPick:String){
+    var storedIndex: Int = 0
+    for i in studentNames.indices{
+        if userPick == studentNames[i]{
+            storedIndex = i
         }
     }
+    print("\(userPick)'s grade in the class is \(finalGrades[storedIndex])")
 }
 
-func calcFinalGrade(foundStudent: String, tempGrades: [String]) {
-    var sumOfScores = 0
+func calcFinalGrade( _ tempGrades: [String]){
+    var sumOfScores: Double = 0
     
     for eachGrade in tempGrades{
-        if let grade = Int(eachGrade){
+        if let grade = Double(eachGrade){
             sumOfScores += grade
         }
     }
-    
-    let gradeInClass = Double(sumOfScores) / Double(tempGrades.count)
-    finalGrades.append(gradeInClass)
-    print("\(foundStudent)'s grade in the class is \(gradeInClass)")
+    finalGrades.append(sumOfScores/Double(tempGrades.count))
 }
 
 func displayAllStudentGrades(){
@@ -128,7 +126,6 @@ func classAverage(){
 func assignmentGradeAverage(){
     
 }
-
 
 func classLowestGrade(){
     
