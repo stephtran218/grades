fa4e67f0da9787270d5bade5ba7ae90369c309d7
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-02-08 10:28:42 -0800

    2/8

--- studentGrades/studentGrades/main.swift
+++ studentGrades/studentGrades/main.swift
@@ -8,13 +8,14 @@
 import Foundation
 import CSV
 
+//These are the global variables that hold arrays of info (roster, grades, index, etc.)
 var studentNames: [String] = []
 var studentGrades: [[String]] = []
 var finalGrades: [Double] = []
 var numOfAssignments: Int = 0
 var storedIndex: Int = 0
 
-
+//This is a safety parameter that prevents errors from crashing the code
 do{
     let stream = InputStream(fileAtPath: "/Users/studentam/Desktop/grades.csv" )
     let csv = try CSVReader(stream: stream!)
@@ -27,23 +28,30 @@ catch{
     print("There was an error trying to read the file")
 }
 
+//This function sorts the original array with names in one, all grades in one, and final grades in one
 func manageData( _ studentInfo: [String]) {
+    //This array will hold just the grades for every assignments as strings
     var tempGrades: [String] = []
+    //This for loop will go in the studentInfo array to look through every indice and sort it out. Typically, the 0-index is just the name so it sorts that in the names array
     for i in studentInfo.indices{
         if i == 0{
             studentNames.append(studentInfo[0])
+            //If it's not in the 0-index, it will be sorted int he tempGrades array which holds all the assignment grades
         } else {
             tempGrades.append(studentInfo[i])
         }
     }
+    //studentGrades is an array thats holding the tempGrades array so that it can look through all the grades in the class
     studentGrades.append(tempGrades)
     calcFinalGrade(tempGrades)
 }
 
 mainMenu()
 
+//this is the main menu function which allows user to look at all the options
 func mainMenu() {
     var menuChoice: String = "0"
+    //This while loop keeps the program going so it doesn't shut down after each function. It will only end when the user puts in "9" to quit
     while menuChoice != "9" {
         print("Welcome to the Grade Manager!\n",
               "What would you like to do? (Enter the number):\n",
@@ -188,6 +196,7 @@ func assignmentGradeAverage() {
             let roundedAverage = (average * 100).rounded() / 100
 
             print("The average grade for assignment \(assignmentNumber) is: \(roundedAverage)")
+            
             print ()
         }  else {
             print("Please enter a valid assignment number between 1 and 10.")
@@ -196,17 +205,63 @@ func assignmentGradeAverage() {
 }
 
 func classLowestGrade(){
+    var lowestGrade: Double = finalGrades[0]
+    var lowestGradeIndex: Int = -1
+    var lowestGradeStudentName = ""
+
+
+    for i in finalGrades.indices {
+        if lowestGrade > finalGrades[i]{
+            lowestGrade = finalGrades[i]
+            lowestGradeIndex = i
+        }
+    }
+    if lowestGradeIndex != -1 && lowestGradeIndex < studentNames.count{
+        lowestGradeStudentName = studentNames[lowestGradeIndex]
+    }
+    print("\(lowestGradeStudentName) is the student with the lowest grade: \(lowestGrade)")
     
+    print ()
 }
 
 func classHighestGrade(){
+    var highestGrade: Double = finalGrades[0]
+    var highestGradeIndex: Int = -1
+    var highestGradeStudentName = ""
+
+    for i in finalGrades.indices {
+        if highestGrade < finalGrades[i]{
+            highestGrade = finalGrades[i]
+            highestGradeIndex = i
+        }
+    }
+    if highestGradeIndex != -1 && highestGradeIndex < studentNames.count{
+        highestGradeStudentName = studentNames[highestGradeIndex]
+    }
+    print("\(highestGradeStudentName) is the student with the highest grade: \(highestGrade)")
     
+    print ()
 }
 
 func studentsByGradeRange(){
+    print("Enter the low range you would like to use:")
     
+    if let lowRangeString = readLine(), let lowRange = Double(lowRangeString) {
+        print("Enter the high range you would like to use:")
+        if let highRangeString = readLine(), let highRange = Double(highRangeString) {
+            for i in finalGrades.indices{
+                let studentInRange = finalGrades[i]
+                if studentInRange >= lowRange && studentInRange <= highRange {
+                    if i < studentNames.count{
+                        print(studentNames[i])
+                    }
+                }
+            }
+            print()
+        }
+    }
 }
 
 func quitMenu(){
-    
+    print ("Have a great rest of your day!")
 }
